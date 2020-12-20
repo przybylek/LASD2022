3aa8fa20b59be5cbf643522389b5420ecc327e1c
SzymonZebrowski <szymekze@gmail.com>
2020-12-20 14:19:44 +0100

    Add topics and interests section

--- pages/index.js
+++ pages/index.js
@@ -5,18 +5,23 @@ import Header from '../sections/Header'
 import About from '../sections/About'
 import PastConferences from '../sections/PastConferences'
 import Sponsors from '../sections/Sponsors'
+import TopicAndInterests from '../sections/TopicsAndInterests'
+
 
 export default function Home() {
   return (
     <Container fluid>
       <Head>
         <title>LASD Conference 2020</title>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"></link>
       </Head>
 
       <Header />
       <About />
+      <TopicAndInterests />
       <PastConferences />
       <Sponsors />
+      
     </Container>
   )
 }
