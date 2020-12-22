760a7d0e30aa82545216bdeae490a68c4b921fe6
LiquidLemon <liquidlemon@protonmail.com>
2020-12-22 15:30:39 +0100

    Fix word breaks in conference links

--- sections/PastConferences.js
+++ sections/PastConferences.js
@@ -3,55 +3,51 @@ import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import Card from 'react-bootstrap/Card'
 
+const conferences = [
+  {
+    title: "LASD@FedCSIS'20",
+    location: "Sofia 2020",
+    website: "https://www.fedcsis.org/2020/lasd",
+    proceedings: "https://annals-csis.org/Volume_21/#H4LASD",
+  },
+  {
+    title: "LASD@FedCSIS'19",
+    location: "Leipzig 2019",
+    website: "https://www.fedcsis.org/2019/lasd",
+    proceedings: "https://annals-csis.org/Volume_18/#H4LASD",
+  },
+  {
+    title: "LASD@FedCSIS'18",
+    location: "Poznan 2018",
+    website: "https://www.fedcsis.org/2018/lasd",
+    proceedings: "https://annals-csis.org/Volume_15/#H4LASD",
+  },
+  {
+    title: "LASD@FedCSIS'17",
+    location: "Prague 2017",
+    website: "https://www.fedcsis.org/2017/lasd",
+    proceedings: "https://annals-csis.org/Volume_11/#H4LASD",
+  },
+]
+
 
 const PastConferences = () => (
   <Container fluid="md" className='section'>
     <a className='target' id='conferences'/>
     <h2 className="display-4 anchor">Past Conferences</h2>
     <Row className="justify-content-md-center m-1">
+      {conferences.map(c => (
       <Col md='auto' className="m-2">
-        <Card style={{ width: '14rem' }} className="mx-auto">
+        <Card style={{ width: '14.5rem' }} className="mx-auto">
           <Card.Body>
-            <Card.Title>LASD@FedCSIS'20</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">Sofia 2020</Card.Subtitle>
-            <Card.Link href="https://www.fedcsis.org/2020/lasd">Website</Card.Link>
-            <Card.Link href="https://annals-csis.org/Volume_21/#H4LASD">Proceedings</Card.Link>
+            <Card.Title>{c.title}</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">{c.location}</Card.Subtitle>
+            <Card.Link href={c.website}>Website</Card.Link>
+            <Card.Link href={c.proceedings}>Proceedings</Card.Link>
           </Card.Body>
         </Card>
       </Col>
-
-      <Col md='auto' className="m-2" sm>
-        <Card style={{ width: '14rem' }} className="mx-auto">
-          <Card.Body>
-            <Card.Title>LASD@FedCSIS'19</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">Leipzig 2019</Card.Subtitle>
-            <Card.Link href="https://www.fedcsis.org/2019/lasd">Website</Card.Link>
-            <Card.Link href="https://annals-csis.org/Volume_18/#H4LASD">Proceedings</Card.Link>
-          </Card.Body>
-        </Card>
-      </Col>
-
-      <Col md='auto' className="m-2">
-        <Card style={{ width: '14rem' }} className="mx-auto">
-          <Card.Body>
-            <Card.Title>LASD@FedCSIS'18</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">Poznan 2018</Card.Subtitle>
-            <Card.Link href="https://www.fedcsis.org/2018/lasd">Website</Card.Link>
-            <Card.Link href="https://annals-csis.org/Volume_15/#H4LASD">Proceedings</Card.Link>
-          </Card.Body>
-        </Card>
-      </Col>
-
-      <Col md='auto' className="m-2">
-        <Card style={{ width: '14rem' }} className="mx-auto">
-          <Card.Body>
-            <Card.Title>LASD@FedCSIS'17</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">Prague 2017</Card.Subtitle>
-            <Card.Link href="https://www.fedcsis.org/2017/lasd">Website</Card.Link>
-            <Card.Link href="https://annals-csis.org/Volume_11/#H4LASD">Proceedings</Card.Link>
-          </Card.Body>
-        </Card>
-      </Col>
+      ))}
     </Row>
   </Container>
 )
