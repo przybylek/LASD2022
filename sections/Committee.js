31e62d052e0c304d4aa10421e91d99a6c6bdf958
Adam Przybylek <adam.przybylek@gmail.com>
2021-03-12 10:33:17 +0100

    PC will be announced later

--- sections/Committee.js
+++ sections/Committee.js
@@ -1,6 +1,17 @@
 import Container from 'react-bootstrap/Container'
 import PeopleList from '../components/PeopleList'
 
+
+const Commitee = () => (
+  <Container className="bg-light section">
+    <a className='target' id='committee'/>
+    <h2 className='display-4 anchor'>Program Commitee</h2>
+    <h3>To be announced later...</h3>
+  </Container>
+)
+
+
+/*
 const Commitee = ({ people }) => (
   <Container className="bg-light section">
     <a className='target' id='committee'/>
@@ -8,5 +19,6 @@ const Commitee = ({ people }) => (
     <PeopleList people={people} />
   </Container>
 )
+*/
 
 export default Commitee
