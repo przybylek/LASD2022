5a70b1f6ed139ceaa9f30635b2554bbc24783577
Adam Przybylek <adam.przybylek@gmail.com>
2021-03-23 14:33:37 +0100

    Sponsorship approved

--- sections/Header.js
+++ sections/Header.js
@@ -1,6 +1,8 @@
 import Container from 'react-bootstrap/Container'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 
+//<span className='badge badge-warning'>pending request</span>
+
 const Header = () => (
   <Jumbotron style={{ backgroundImage: `url(/landingPage.jpg)`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center' }}  className='mt-5'>
     <a className='target' id='home'/>
@@ -12,7 +14,7 @@ const Header = () => (
     </h2>
     <p className='text-center lead'>22 January, 2022, Online event<sup>*</sup></p>
     <p className='text-center lead text-info font-italic'>Free of charge, with Springer LNBIP proceedings!</p>
-    <p className='text-center lead text-info font-italic'>Corresponding authors of 20 top-rated accepted papers will get complimentary 1 year membership in <a href='https://www.agilealliance.org'>Agile Alliance</a>! <span className='badge badge-warning'>pending request</span></p>
+    <p className='text-center lead text-info font-italic'>Corresponding authors of 15 top-rated accepted papers will get complimentary 1 year membership in <a href='https://www.agilealliance.org'>Agile Alliance</a>!</p>
     <p className='text-center lead'><small><sup>*</sup>The conference will consist of pre-recorded presentations, live keynote, and Q&A sessions.</small></p>
   </Jumbotron>
 )
