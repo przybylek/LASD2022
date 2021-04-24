6d91fa36ab22fa3e55f81c2e53417eeff8be63c2
Adam Przybylek <adam.przybylek@gmail.com>
2021-04-24 10:17:11 +0200

    Keynotes added

--- sections/Keynotes.js
+++ sections/Keynotes.js
@@ -5,7 +5,35 @@ const KeynoteSpeakers = () => (
     <a className='target' id='keynotes'/>
     <h2 className='display-4 anchor'>Keynotes</h2>
 
-<h3>To be announced later...</h3>
+    <h3>Markus Borg</h3>
+/*
+
+    <dl>
+      <dt>Keynote title</dt>
+      <dd></dd>
+
+      <dt>Short Biography</dt>
+      <dd>
+      </dd>
+    </dl>
+*/
+
+    <h3>Raman Ramsin</h3>
+    <dl>
+      <dt>Keynote title</dt>
+      <dd>
+        The promises of model-driven development in an agile context
+      </dd>
+*/
+      <dt>Short Biography</dt>
+      <dd>
+        <p>
+        </p>
+      </dd>
+*/
+
+    </dl>
+
 
   </Container>
 )
@@ -13,50 +41,5 @@ const KeynoteSpeakers = () => (
 export default KeynoteSpeakers
 
 
-/*
-    <h3>Philipp Diebold</h3>
-    <dl>
-      <dt>Keynote title</dt>
-      <dd>Agility Yesterday, Today & Tomorrow</dd>
-
-      <dt>Short Biography</dt>
-      <dd>
-        Before starting his own consulting, coaching and training company, called <a href="https://www.bagilstein.de/">Bagilstein</a>, Philipp was working as a project manager and researcher at the Fraunhofer Institut for Experimental Software Engineering IESE.
-        Working in the area of agile processes, methods and practices, he published a lot around this topics in different conferences, journals and books. He finished his PhD on identifying the right degree of agility at the Technical University of Kaiserslautern.
-        With his own company his is now coaching different customers for more than 2 years in agile transformations still keeping the eye on the leading edge of research.
-      </dd>
-    </dl>
-
-    <h3>Sanjay Misra</h3>
-    <dl>
-      <dt>Keynote title</dt>
-      <dd>
-        Pair Programming: An Empirical investigation in an Agile Software
-        Development environment
-      </dd>
-
-      <dt>Short Biography</dt>
-      <dd>
-        <p>
-          <a href="https://covenantuniversity.edu.ng/Profiles/Misra-Sanjay">Sanjay Misra</a> is full Professor of Computer(software) Engineering at Covenant University (<a href="https://www.timeshighereducation.com/world-university-rankings/covenant-university">400-500 ranked University</a>) Ota, Nigeria.
-          He has 25 years of wide experience in academic administration and research in various universities in Asia, Europe, and Africa. He is PhD. in Information and Know. Engg (Software Engineering) from the University of Alcala, Spain and M.Tech. (Software Engineering) from Motilal Nehru National Institute of Technology, India.
-          As of today (15.03.2020) - as per SciVal (SCOPUS - Elsevier analysis) - he is the most productive researcher <a href="https://www.scival.com/overview/authors?uri=Country/566">(no. 1-)</a> in whole Nigeria during 2012-2017, 2013-2018 and 2014-2019 (<a href="https://t.co/fBYnVxbmiL">in all subjects</a>), in computer science <a href="https://www.scival.com/overview/authors?uri=Country/566"> no 1 in the whole country</a> and <a href="https://www.scival.com/overview/authors?uri=CountryGroup/1">no 4 in the whole continent</a>.
-          Total around <a href="https://scholar.google.com/citations?user=AFjvjTgAAAAJ&amp;hl=en"> 400 articles</a> (SCOPUS/Web of Science) with 200 coauthors around the world (-90 in JCR/SCIE Journals) in the core & application area of Software Engg (SQA, SPI, SPM), Web engg, Health Informatics, Intelligent systems etc.
-        </p>
-
-        <p>
-          He has delivered more than 80 keynote speeches/invited talks/public lectures in reputed conferences and institutes around the world (travelled around 60 countries).
-          He got several awards for outstanding publications (<a href="http://digital-library.theiet.org/content/journals/iet-sen/info/prizes;jsessionid=73grefh54gcl.x-iet-live-01">2014 IET Software Premium Award</a> (UK)), and from TUBITAK-Turkish Higher Education, and Atilim University). He edited (with colleagues) 42 LNCS & 6 IEEE proceedings, several books, editor in chief of book series <a href="https://www.igi-global.com/book-series/advances-personnel-project-management/77666">IT Personnel and Project Management</a>, <a href="https://www.igi-global.com/journal/international-journal-human-capital-information/1152">International Journal of Human Capital and Information Technology Professionals</a> (IJHCITP)-IGI Global, and of 3 journals (IJ) and editor in various SCIE journals.
-        </p>
-
-        <p>
-          He is a software engineer and previously held academic positions at FUT Minna - Nigeria (as Head Department of Computer Engineering and Head- Department of Cyber Security), Atilim University - Turkey, Subharati University and UP Technical University - India. He is also visiting/Collaborative professor at the University of Alcala – Spain (since 2011-), UCV- Valparaiso – Chile (since 2013-), UNICEN, Tandil – Argentina (since 2011-), and Atilim University – Turkey (since 2011-).
-        </p>
-
-        <p>
-          His current research covers the areas of software quality assurance, software process improvement, software project management, object-oriented technologies, XML, SOA, Web services, cognitive informatics, artificial intelligence, neural network, health Informatics, e-learning, cloud computing, and cybersecurity. Prof Sanjay is coordinating/working/collaborating with several research groups (as visiting/adjunct professor/collaborative researcher) in various universities and industry around the world (Spain, Argentina, Turkey, Singapore, Chile, Brazil, Mexico, Norway, France, South Africa, Malaysia, Nigeria, Myanmar, Vietnam, India).
-        </p>
-      </dd>
-    </dl>
-*/
+//<h3>To be announced later...</h3>
 
