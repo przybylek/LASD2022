232f2742379f7b8606fac3d3892d015c67465535
Adam Przybylek <adam.przybylek@gmail.com>
2021-04-24 10:22:24 +0200

    Keynotes fixed

--- sections/Keynotes.js
+++ sections/Keynotes.js
@@ -6,6 +6,24 @@ const KeynoteSpeakers = () => (
     <h2 className='display-4 anchor'>Keynotes</h2>
 
     <h3>Markus Borg</h3>
+
+    <h3>Raman Ramsin</h3>
+    <dl>
+      <dt>Keynote title</dt>
+      <dd>
+        The promises of model-driven development in an agile context
+      </dd>
+    </dl>
+
+
+  </Container>
+)
+
+export default KeynoteSpeakers
+
+
+//<h3>To be announced later...</h3>
+
 /*
 
     <dl>
@@ -18,28 +36,4 @@ const KeynoteSpeakers = () => (
     </dl>
 */
 
-    <h3>Raman Ramsin</h3>
-    <dl>
-      <dt>Keynote title</dt>
-      <dd>
-        The promises of model-driven development in an agile context
-      </dd>
-*/
-      <dt>Short Biography</dt>
-      <dd>
-        <p>
-        </p>
-      </dd>
-*/
-
-    </dl>
-
-
-  </Container>
-)
-
-export default KeynoteSpeakers
-
-
-//<h3>To be announced later...</h3>
 
