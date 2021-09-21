15f747cd3c39808c4d919589476017d88e23a0a6
Adam Przybylek <adam.przybylek@gmail.com>
2021-09-21 12:23:06 +0200

    Committee updated

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ chairs, committee, reviewers }) {
   return (
     <Container fluid className='px-0'>
       <Head>
-        <title>LASD Conference 2020</title>
+        <title>LASD 2022</title>
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossOrigin="anonymous"></link>
       </Head>
 
--- sections/Submissions.js
+++ sections/Submissions.js
@@ -50,7 +50,7 @@ const Submissions = () => (
     </p>
     <dl>
       <dt>Zoom</dt>
-      <dd className="ml-4"><a href="https://support.zoom.us/hc/en-us/articles/201362473-Local-Recording">Local recording</a>, <a href="https://ieeetv.ieee.org/mobile/video/recording-your-presentation-with-zoom">IEEE.tv</a></dd>
+      <dd className="ml-4"><a href="https://support.zoom.us/hc/en-us/articles/201362473-Local-Recording">Local recording</a></dd>
       <dt>PowerPoint</dt>
       <dd className="ml-4"><a href="https://support.microsoft.com/en-us/office/record-a-slide-show-with-narration-and-slide-timings-0b9502c6-5f6c-40ae-b1e7-e47d8741161c?ui=en-us&rs=en-us&ad=us">Record a slide show with narration and slide timings</a>, <a href="https://www.youtube.com/watch?v=D8JV3w4TOVw">How to Make a Video in PowerPoint</a></dd>
       <dt>KeyNote</dt>
