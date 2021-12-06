da86d042421a86011aba4b714d4ea3e5b5267daa
Adam Przybylek <adam.przybylek@gmail.com>
2021-12-06 20:10:48 +0100

    Keynote update

--- sections/Keynotes.js
+++ sections/Keynotes.js
@@ -11,6 +11,9 @@ const KeynoteSpeakers = () => (
       <dd>
         Agility in Software 2.0 - Notebook Interfaces and MLOps
       </dd>
+      <dt>Short Biography</dt>
+      <dd>Dr. Markus Borg is a senior researcher with RISE Research Institutes of Sweden AB. He is also an adjunct lecturer at Lund University from where he obtained a PhD in software engineering in 2015. His research interests include empirical software engineering, machine learning, and software testing. Before embarking on the research career, Markus worked at ABB as a software engineer in safety-critical process automation. Markus is also a board member of Swedsoft, an independent non-profit organization with the mission to increase the competitiveness of Swedish software.
+      </dd>
     </dl>
 
 
