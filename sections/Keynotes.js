d330579fc5de481b1501baac13d4c21f66f750f1
Adam Przybylek <adam.przybylek@gmail.com>
2021-12-08 12:41:20 +0100

    Keynote II update

--- sections/Keynotes.js
+++ sections/Keynotes.js
@@ -8,20 +8,18 @@ const KeynoteSpeakers = () => (
     <h3>Markus Borg</h3>
     <dl>
       <dt>Keynote title</dt>
-      <dd>
-        Agility in Software 2.0 - Notebook Interfaces and MLOps
-      </dd>
+      <dd>Agility in Software 2.0 – Notebook Interfaces and MLOps with Buttresses and Rebars</dd>
       <dt>Short Biography</dt>
       <dd>Dr. Markus Borg is a senior researcher with RISE Research Institutes of Sweden AB. He is also an adjunct lecturer at Lund University from where he obtained a PhD in software engineering in 2015. His research interests include empirical software engineering, machine learning, and software testing. Before embarking on the research career, Markus worked at ABB as a software engineer in safety-critical process automation. Markus is also a board member of Swedsoft, an independent non-profit organization with the mission to increase the competitiveness of Swedish software.
       </dd>
     </dl>
 
-
     <h3>Raman Ramsin</h3>
     <dl>
       <dt>Keynote title</dt>
-      <dd>
-        The promises of model-driven development in an agile context
+      <dd>Promises of Model-Driven Development in an Agile Context</dd>
+      <dt>Short Biography</dt>
+      <dd>Dr. Raman Ramsin is with Sharif University of Technology, Iran. He received his PhD in Computer Science from the University of York, UK, in 2006. As a lecturer, his graduate and undergraduate courses span the process-related areas of software engineering, including software development methodologies, patterns in software engineering, object-oriented analysis and design, and agile methods. His recent research focuses on situational method engineering, agile development, and model-driven engineering.
       </dd>
     </dl>
 
